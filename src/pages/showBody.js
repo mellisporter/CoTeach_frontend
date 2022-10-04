52a8786b55c0096be6e1ef0910052940989fa1cf
Matt Porter <matthew.ellis.porter@gmail.com>
2022-10-04 10:07:25 -0400

    delete button tested and functions

--- src/pages/showBody.js
+++ src/pages/showBody.js
@@ -1,17 +1,24 @@
 import VideoPlayer from "../components/VideoPlayer";
 import Tabs from "../components/Tabs";
 function ShowBody(props) {
+    const id = props.match.params.id;
+    const content = props.content;
+    const lesson = content.find(l => l._id === id);
 
-    const removeContent = (props) => {
-        props.deleteContent(props.content._id);
+    const removeContent = () => {
+        props.deleteContent(lesson._id);
         props.history.push("/");
       }
+
     return (
         <div className="ShowContainer">
             <div className="ShowVideo">
                 <div className="ShowVideoPlayer"><VideoPlayer /></div>
+                <button id="delete" onClick={removeContent}>
+                    Delete Content
+                </button>
                 <div className="SuggestedContent">SuggestedContent</div>
-                <button id="delete" onClick={removeContent}>Delete Content</button>
+                
             </div>
 
             <div className="ShowContent"><Tabs /></div>
